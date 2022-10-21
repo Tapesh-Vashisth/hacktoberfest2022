2957ac48937cf4b6cd8c29522cba545b26e15926
gummithaanushka <101972595+gummitha-anushka@users.noreply.github.com>
2022-10-21 20:59:20 +0530

    Update Leapyear.c

--- c/Leapyear.c
+++ c/Leapyear.c
@@ -5,23 +5,10 @@ int main() {
    scanf("%d", &year);
 
    // leap year if perfectly divisible by 400
-   if (year % 400 == 0) {
+   if (year % 400 == 0 || (year%4==0 && year%100!=0))
       printf("%d is a leap year.", year);
-   }
-   // not a leap year if divisible by 100
-   // but not divisible by 400
-   else if (year % 100 == 0) {
+   else
       printf("%d is not a leap year.", year);
-   }
-   // leap year if not divisible by 100
-   // but divisible by 4
-   else if (year % 4 == 0) {
-      printf("%d is a leap year.", year);
-   }
-   // all other years are not leap years
-   else {
-      printf("%d is not a leap year.", year);
-   }
 
    return 0;
 }
