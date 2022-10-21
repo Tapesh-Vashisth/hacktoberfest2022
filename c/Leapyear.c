9aaa61bc5889cadb3e4c3d64b86c31492fa1b409
gummithaanushka <101972595+gummitha-anushka@users.noreply.github.com>
2022-10-21 21:06:44 +0530

    Update Leapyear.c

--- c/Leapyear.c
+++ c/Leapyear.c
@@ -3,8 +3,7 @@ int main() {
    int year;
    printf("Enter a year: ");
    scanf("%d", &year);
-
-   // leap year if perfectly divisible by 400
+\\Year entered will be leap only when it is divisible by 4 and 400 and not only by 100. 
    if (year % 400 == 0 || (year%4==0 && year%100!=0))
       printf("%d is a leap year.", year);
    else
